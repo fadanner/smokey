3572be1f078c2b7f353ebb90f0319f5acbca2199
fadanner <fadanner@gmail.com>
2020-05-07 14:42:14 +0200

    Added Fan
    Added Fan class and AirInfo Frame

--- smokey/smokey.ino
+++ smokey/smokey.ino
@@ -8,6 +8,53 @@ SSD1306 display (0x3c, 5, 4);
 OLEDDisplayUi ui     ( &display );
 
 
+
+class Fan{
+  private:
+    String _name;
+    byte _speed = 0;
+    byte _pwmPin = 0;
+    byte _switchPin = 2;
+    byte _pwmChannel = 0;
+    byte _pwmResolution = 8;
+    int _pwmFreq = 25000;
+   public:
+    Fan(char* name, byte pwmPin, byte switchPin, byte pwmChannel = 0, byte pwmResolution = 8, int pwmFreq = 25000){
+      _name = name;
+      _pwmPin = pwmPin;
+      _switchPin = switchPin;
+      _pwmChannel = pwmChannel;
+      _pwmResolution = pwmResolution;
+      _pwmFreq = pwmFreq; 
+    }
+
+    void init(){
+      pinMode(_pwmPin,OUTPUT);
+      pinMode(_switchPin,OUTPUT);
+      ledcSetup(_pwmChannel,_pwmFreq,_pwmResolution);
+      ledcAttachPin(_pwmPin,_pwmChannel);
+    }
+
+    byte getSpeed(){
+      return _speed;
+    }
+    String getName(){
+      return _name;
+    }
+    void setSpeed(byte speed){
+      _speed = speed;
+      float setSpeed = 170-(_speed / 100.0 * 170);
+      ledcWrite(_pwmChannel,setSpeed);
+      if(speed > 0){
+        digitalWrite(_switchPin, HIGH);
+      }else{
+        digitalWrite(_switchPin, LOW); 
+      }
+    }
+
+};
+
+
 class TempSensor{
   private:
     long _value;
@@ -54,6 +101,10 @@ TempSensor sensors[sensorCount] = {
   TempSensor("Sensor4",15)
 };
 
+const int fanPwmPin = 0;
+const int fanSwitchPin = 2;
+Fan fan = Fan("Fan",0,2);
+
 
 void controllerInfos(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
   // Demonstrates the 3 included default sizes. The fonts come from SSD1306Fonts.h file
@@ -79,6 +130,15 @@ void sensorInfos(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int
   }
 }
 
+void airInfos(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
+  // Demonstrates the 3 included default sizes. The fonts come from SSD1306Fonts.h file
+  // Besides the default fonts there will be a program to convert TrueType fonts into this format
+  display->setTextAlignment(TEXT_ALIGN_LEFT);
+  display->setFont(ArialMT_Plain_10);
+  display->drawString(0, 0, fan.getName());
+  display->drawString(60, 0, String(fan.getSpeed())); 
+}
+
 
 void msOverlay(OLEDDisplay *display, OLEDDisplayUiState* state) {
   display->setTextAlignment(TEXT_ALIGN_RIGHT);
@@ -86,8 +146,8 @@ void msOverlay(OLEDDisplay *display, OLEDDisplayUiState* state) {
   display->drawString(128, 0, String(millis()));
 }
 
-FrameCallback frames[] = {controllerInfos,sensorInfos};
-int frameCount = 2;
+FrameCallback frames[] = {controllerInfos,sensorInfos,airInfos};
+int frameCount = 3;
 OverlayCallback overlays[] = { msOverlay };
 int overlaysCount = 1;
 /////////////////////////////////////////////////////
@@ -102,6 +162,7 @@ void setup() {
   ui.setFrames(frames, frameCount);
   ui.setTimePerFrame(5*1000);
   ui.init();
+  fan.init();
    WiFi.begin(ssid, password);
    while (WiFi.status() != WL_CONNECTED) {
           delay(500);
@@ -119,6 +180,12 @@ void loop() {
   for(int i = 0; i < sensorCount; i++){
     sensors[i].measure();
   }
+    if(Serial.available() > 0){
+      byte currentSpeed = (String(Serial.readStringUntil('\n'))).toInt();
+      fan.setSpeed(currentSpeed);
+      Serial.println(currentSpeed);
+    }
+
   ui.update();
   delay(10);
 
